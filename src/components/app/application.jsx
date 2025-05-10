1d42fcb1e9b3892e094807e2d27de2cae32558c5
narukaz <85193271+narukaz@users.noreply.github.com>
2025-05-10 15:14:04 +0530

    change

--- src/components/app/application.jsx
+++ src/components/app/application.jsx
@@ -65,7 +65,7 @@ export default function Application() {
               </div>
               <AnimatePresence mode="wait">
                 <motion.div
-                  key={location.pathname}
+                  key={currentaddress.pathname}
                   initial={{ opacity: 0, x: 50 }}
                   animate={{ opacity: 1, x: 0 }}
                   exit={{ opacity: 0, x: -10 }}
